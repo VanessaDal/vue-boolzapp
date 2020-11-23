0fe6cd0d4abf96442234f375ce5726370ba2719a
Vanessa D'Alessandro <leavesthatfall@gmail.com>
2020-11-23 17:46:08 +0100

    vue wip

--- js/script.js
+++ js/script.js
@@ -5,55 +5,51 @@ var app=new Vue({
             nome:"Billy Ballo",
             anteprima:"ok...",
             img:"img/avatar_2.jpg",
+            accesso:"22/11/2020 16:15",
             chat:[{
                 ricevuto:"ciao sono billy ballo"
+            }]
             },
             {
             nome:"Herbert Ballerina",
             anteprima:"ok...",
             img:"img/avatar_3.jpg",
+            accesso:"22/11/2020 16:15",
             chat:[{
                 ricevuto:"ciao sono herbert"
+            }]
             },
             {
             nome:"Anna Pannocchia",
             anteprima:"ok...",
             img:"img/avatar_4.jpg",
+            accesso:"22/11/2020 16:15",
             chat:[{
             ricevuto:"ciao sono Anna"
+            }]
             },
             {
             nome:"Riccardino Fuffolo",
             anteprima:"ok...",
             img:"img/avatar_5.jpg",
+            accesso:"22/11/2020 16:15",
             chat:[{
                 ricevuto:"ciao sono Riccardino"
             }]
+            }
+        ]
+        }
         
     
-        }],
+ 
         
-        
-    },
-    methods:{
-        submitText:function(){
-            if(this.todo.includes(this.text)===false){
-            this.todo.push(this.text);
-            this.text="";
-            console.log(this.todo)}
-            else{alert("This task already exist")}
-        },
+    // methods:{
+    //     submitText:function(){
+    //         if(this.todo.includes(this.text)===false){
+    //         this.todo.push(this.text);
+    //         this.text="";
+    //         console.log(this.todo)}
+    //         else{alert("This task already exist")}
+    //     },
 
-        // removeText:function(i){
-        //     this.todo.splice(i,1)
-        // },
-
-        // editText:function(i){
-        //     var a=prompt("type a new text");
-        //     // this.todo.splice(i,1);
-        //     // this.todo.push(a)
-        //     this.todo[i]=a;
-        //     this.$forceUpdate()
-        // }
-    }
 })
